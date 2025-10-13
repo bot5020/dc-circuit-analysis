f170b4d3e1f878935655fc918d93c8dab319de54
stepprog <stepan.mokeev160808@gmail.com>
2025-10-13 23:42:39 +0300

    ready pls

--- config/training_config.py
+++ config/training_config.py
@@ -24,10 +24,10 @@ class TrainingConfig:
     lora_target_modules: List[str] = None
     
     # Обучение
-    learning_rate: float = 2e-5  #
+    learning_rate: float = 2e-5
     max_steps: int = 200 
     save_steps: int = 50 
-    batch_size: int = 8 
+    batch_size: int = 16 
     gradient_accumulation_steps: int = 1  
     
     # Оптимизатор
@@ -40,7 +40,7 @@ class TrainingConfig:
     # Генерация
     max_seq_length: int = 11000
     max_completion_length: int = 11000
-    num_generations: int = 4
+    num_generations: int = 8
     temperature: float = 0.7
     top_p: float = 0.95
     repetition_penalty: float = 1.1
